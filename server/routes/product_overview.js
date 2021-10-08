ae1fc29a359593b9905adc7cae4590fe57badb53
Ryan Lott <ryanclott@outlook.com>
2021-10-07 20:38:29 -0500

    Product Overview Updates

--- server/routes/product_overview.js
+++ server/routes/product_overview.js
@@ -1,23 +1,23 @@
 const axios = require('axios');
 const router = require('express').Router();
 const bodyParser = require('body-parser');
-const { apiToken, apiURL } = require('./../../config.js');
+const { apiToken, apiURL, SDC_API_URL } = require('./../../config.js');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/productStyles', (req, res) => {
-  axios.get('http://localhost:3000/test')
-  .then(data => {
-    console.log('Data received from API', data.data);
-  })
-  .catch(err => {
-    console.log('Err received from API',err);
-  });
-  axios.get(`${apiURL}products/${req.query.productId}/styles`, {
-    headers: {
-      Authorization: apiToken
-    }
+  // axios.get('http://localhost:3000/test')
+  // .then(data => {
+  //   console.log('Data received from API', data.data);
+  // })
+  // .catch(err => {
+  //   console.log('Err received from API',err);
+  // });
+  axios.get(`${SDC_API_URL}products/${req.query.productId}/styles`, {
+    // headers: {
+    //   Authorization: apiToken
+    // }
   })
     .then((response) => {
       res.status(200).send(response.data);
@@ -29,10 +29,10 @@ router.get('/productStyles', (req, res) => {
 });
 
 router.get('/product', (req, res) => {
-  axios.get(`${apiURL}products/${req.query.productId}`, {
-    headers: {
-      Authorization: apiToken
-    }
+  axios.get(`${SDC_API_URL}products/${req.query.productId}`, {
+    // headers: {
+    //   Authorization: apiToken
+    // }
   })
     .then((response) => {
       res.status(200).send(response.data);
@@ -43,16 +43,42 @@ router.get('/product', (req, res) => {
     });
 });
 
+// single query endpoint
+router.get('/productData', (req, res) => {
+  console.log(req);
+  axios.get(`${SDC_API_URL}products/${req.query.productId}`, {
+    // headers: {
+    //   Authorization: apiToken
+    // }
+  })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      console.log('error in axios product', err);
+      res.status(400).send('error in app.get product info', err);
+    });
+});
+
+
+
+
+
+
 router.post('/cart', (req, res) => {
+  req.body['sku_id'] = parseInt(req.body['sku_id']);
+  console.log(req.body);
   axios.post(`${apiURL}cart`, req.body, {
     headers: {
       Authorization: apiToken
     }
   })
     .then((response) => {
+      console.log(`Response: ${response}`);
       res.status(200).send(response.data);
     })
     .catch((err) => {
+      console.log(`Errors: ${err}`);
       res.status(400).send(err);
     });
 });
